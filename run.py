43a0b89f7fdc7f3ad17323125646173166119364
GerardoSaldivar <gerardo.saldivar.10@gmail.com>
2022-08-16 18:34:22 -0500

    cambiando ruta del index

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('https://risc-v.herokuapp.com/', methods = ['GET'])
+@app.route('/', methods = ['GET'])
 def index():
     return render_template('index.html')
 
